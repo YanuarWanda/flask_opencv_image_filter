6973bf57077e68026dd388af3923438c01caf042
yanuarwanda <yanuar.wanda2@gmail.com>
2022-02-11 17:50:15 +0700

    feat: add bright & dark filter

--- website/views.py
+++ website/views.py
@@ -7,19 +7,29 @@ def grayscale(img):
     grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return grayscale
 
+def setBrighness(img, beta_value):
+    result = cv2.convertScaleAbs(img, beta = beta_value)
+    return result
+
 views = Blueprint("views", __name__)
 
 @views.route("/", methods = ['GET', 'POST'])
 def home():
     if (request.method == "POST"):
+        if not request.files:
+            return make_response({ "status": "error" }), 400
+
         imageBuffer = request.files["image"].read()
         data = np.frombuffer(imageBuffer, np.uint8)
         image = cv2.imdecode(data, 1)
 
         filter = request.form.get("filter")
-        match filter:
-            case "1":
-                result = grayscale(image)
+        if (filter == "1"):
+            result = grayscale(image)
+        elif (filter == "2"):
+            result = setBrighness(image, 60)
+        elif (filter == "3"):
+            result = setBrighness(image, -60)
 
         image = cv2.imencode('.png', result)[1]
 
